f3af4dc842c75e2b4e077014ad4d30d44a200fb3
peperc <jose.reverte.cazorla@gmail.com>
2022-05-16 20:06:29 +0200

    database creation done script

--- dataset_extractor.py
+++ dataset_extractor.py
@@ -1,22 +1,23 @@
 import cv2
 import dlib
-import keyboard
-import logging
+import time
 import os
 
 import weka.core.jvm as jvm
 
 from weka.core.converters import Saver
-from lib.pupil_tracker import face_shape_to_array, get_and_draw_pupils
+from lib.pupil_tracker import face_shape_to_array, get_and_draw_pupils, on_threshold_change
 from lib.weka_classifier import create_dataset, add_to_dataset
 
-
-logging.basicConfig(level=logging.ERROR)
 
 # With the detector, we get faces from frames represented as rectangles 
 faces_detector = dlib.get_frontal_face_detector()
 # With the predictor, we get the 68 points representing the face from the face_detector's rectangles
-face_predictor = dlib.shape_predictor('shape_68.dat')
+face_predictor = dlib.shape_predictor(os.path.join(os.path.dirname(__file__), 'lib/models/shape_68.dat'))
+
+# Create the threshold window
+cv2.namedWindow('Threshold')
+cv2.createTrackbar('Value', 'Threshold', 0, 255, on_threshold_change)
 
 # Create the eyes tracker window
 cv2.namedWindow('Tracker')
@@ -26,11 +27,10 @@ jvm.start()
 dataset = create_dataset()
 
 # Starts the webcam
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-key = ''
-while(key != ord('q')):
-    key = ord(keyboard.read_key())
+key = -1
+while(key != 27):
     ret, frame = video.read()
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -43,14 +43,22 @@ while(key != ord('q')):
 
         frame, thresh, left_eye, right_eye = get_and_draw_pupils(frame, face_array)
 
-        add_to_dataset(dataset, key, left_eye, right_eye)
+        if key != -1:
+            add_to_dataset(dataset, key, left_eye, right_eye)
+            print(f'Instance added to dataset: {key}, {str(left_eye)}, {str(right_eye)}')
 
     # Show the final frames
     cv2.imshow('Tracker', frame)
-    cv2.waitKey(1)
+    cv2.imshow("Threshold", thresh)
+    
+    key = cv2.waitKey(1)
+
+t = time.localtime()
+current_time = time.strftime("%H-%M-%S", t)
 
 saver = Saver(classname="weka.core.converters.ArffSaver")
-saver.save_file(dataset, os.path.dirname(__file__).join("pressed_keys.arff"))
+saver.save_file(dataset, os.path.join(os.path.dirname(__file__), f'datasets/{current_time}.arff'))
 
 jvm.stop()
+video.release()
 cv2.destroyAllWindows()
